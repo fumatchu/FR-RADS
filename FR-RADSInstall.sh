3e76581251ec7878194624e81c159728467ae39f
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-01-22 12:20:30 -0500

    Create FR-RADSInstall.sh

---
+++ FR-RADSInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#install.sh
+echo "**************************"
+echo "Please wait while we gather some files"
+echo "**************************"
+
+echo " "
+echo "Installing wget and git"
+echo " "
+
+dnf -y install wget git 
+
+echo " "
+echo "Retreiving Files from github"
+
+mkdir /root/ADDCInstaller
+
+git clone https://github.com/fumatchu/FR-RADS.git /root/FRRADSInstaller
+
+chmod 700 /root/FRRADSInstaller/i*
+clear
+
+/root/FRRADSInstaller/install1.sh
