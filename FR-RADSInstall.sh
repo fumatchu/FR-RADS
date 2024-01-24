c36a3755b6ae944f0db8e9817b9d432700e20ee2
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-01-24 09:45:17 -0500

    Update FR-RADSInstall.sh

--- FR-RADSInstall.sh
+++ FR-RADSInstall.sh
@@ -13,11 +13,11 @@ dnf -y install wget git
 echo " "
 echo "Retreiving Files from github"
 
-mkdir /root/ADDCInstaller
+mkdir /root/FR-Installer
 
 git clone https://github.com/fumatchu/FR-RADS.git /root/FR-Installer
 
 chmod 700 /root/FR-Installer/i*
 clear
 
-/root/FRRADSInstaller/install1.sh
+/root/FR-Installer/install1.sh
