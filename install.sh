5b7797328765a13cfc4176f4135ac8b57ee8ac5e
Meraki Matt <47304651+fumatchu@users.noreply.github.com>
2024-03-12 07:05:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -177,7 +177,7 @@ dnf -y install epel-release
 dnf -y install dnf-plugins-core
 dnf config-manager --set-enabled crb
 dnf -y update
-dnf -y install ntsysv wget open-vm-tools oddjob oddjob-mkhomedir samba-winbind samba-winbind-clients samba-common-tools freeradius freeradius-utils realmd bind-utils
+dnf -y install ntsysv wget open-vm-tools oddjob oddjob-mkhomedir samba-winbind samba-winbind-clients samba-common-tools freeradius freeradius-utils realmd bind-utils dmidecode
 systemctl disable iscsi
 systemctl disable iscsi-onboot
 clear
